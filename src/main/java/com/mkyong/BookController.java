b25639bedfda2afec1c7490dc1642c21aa6a2678
aneelachalam <62289487+aneelachalam@users.noreply.github.com>
2020-06-16 11:38:48 +0530

    testing

--- src/main/java/com/mkyong/BookController.java
+++ src/main/java/com/mkyong/BookController.java
@@ -23,7 +23,7 @@ public class BookController {
     }
     @GetMapping("/test")
     String test() {
-        return "Testing Demo ...12";
+        return "Testing Demo ...13";
     }
 
     // Save
